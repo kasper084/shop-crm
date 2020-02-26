ee3565f15d04725401a723437cb0d8b04d5482f2
kasper084 <kasper084@gmail.com>
2020-02-26 12:49:16 +0200

    start

---
+++ src/test/java/some/shop/com/shopcrm/ShopCRMApplicationTests.java
@@ -0,0 +1,13 @@
+package some.shop.com.shopcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCRMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
